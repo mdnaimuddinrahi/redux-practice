81fad85e9aa20043a5b5442aea9f6bc9e239242d
mdnaimuddinrahi <mdnaimuddinrahi@gmail.com>
2026-03-10 09:54:51 +0600

    card list

--- src/components/GroceryApp/CardItem.jsx
+++ src/components/GroceryApp/CardItem.jsx
@@ -1,28 +1,28 @@
-const CardItem = () => {
+const CardItem = ({ item }) => {
     return(
         <div className="cartCard">
             <div className="flex items-center col-span-6 space-x-6">
-                <img className="lws-cartImage" src="https://i.dummyjson.com/data/products/40/thumbnail.jpg" alt="product" />
+                <img className="cartImage" src={item.image} alt={item.name} />
                 <div className="space-y-2">
-                    <h4 className="lws-cartName">Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptop</h4>
-                    <p className="lws-cartCategory">Men's clothing</p>
-                    <p>BDT <span className="lws-cartPrice">1100</span></p>
+                    <h4 className="cartName">{item.name}</h4>
+                    <p className="cartCategory">{item.category}</p>
+                    <p>BDT <span className="cartPrice">{item.price}</span></p>
                 </div>
             </div>
             <div className="flex items-center justify-center col-span-4 mt-4 space-x-8 md:mt-0">
                 <div className="flex items-center space-x-4">
-                    <button className="lws-incrementQuantity">
+                    <button className="incrementQuantity">
                         <i className="text-lg fa-solid fa-plus"></i>
                     </button>
-                    <span className="lws-cartQuantity">2</span>
-                    <button className="lws-decrementQuantity">
+                    <span className="cartQuantity">{item.quantity}</span>
+                    <button className="decrementQuantity">
                         <i className="text-lg fa-solid fa-minus"></i>
                     </button>
                 </div>
-                <p className="text-lg font-bold">BDT <span className="lws-calculatedPrice">2200</span></p>
+                <p className="text-lg font-bold">BDT <span className="calculatedPrice">{item.price * item.quantity}</span></p>
             </div>
             <div className="flex items-center justify-center col-span-2 mt-4 md:justify-end md:mt-0">
-                <button className="lws-removeFromCart">
+                <button className="removeFromCart">
                     <i className="text-lg text-red-400 fa-solid fa-trash"></i>
                 </button>
             </div>
--- src/components/GroceryApp/Checkout.jsx
+++ src/components/GroceryApp/Checkout.jsx
@@ -1,13 +1,18 @@
 import CardItem from "./CardItem";
 import Billdetail from "./Billdetail";
+import { useSelector } from "react-redux";
 const Checkout = () => {
+    const cartItems = useSelector((state) => state.grocery.cart);
+
     return(
         <main className="py-16">
             <div className="container 2xl:px-8 px-2 mx-auto">
                 <h2 className="mb-8 text-xl font-bold">Shopping Cart</h2>
                 <div className="cartListContainer">
                     <div className="space-y-6">
-                        <CardItem />
+                    {cartItems.map((item) => (
+                        <CardItem key={item.id} item={item} />
+                    ))}
                     </div>
                     <div>
                         <Billdetail />
