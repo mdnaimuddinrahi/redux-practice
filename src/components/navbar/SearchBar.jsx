29db44bb4edca0bd0159fc162a84df709b1a72fb
mdnaimuddinrahi <mdnaimuddinrahi@gmail.com>
2026-03-20 10:11:04 +0600

    filter state management

--- src/components/navbar/SearchBar.jsx
+++ src/components/navbar/SearchBar.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { searched } from "../../features/filter/filterSlice";
 
 export default function SearchBar() {
+  const dispatch = useDispatch()
+  const [input, setInput] = useState()
+  const {search} = useSelector(state => state.filters)
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(searched(input))
+  }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
         <input
             className="outline-none border-none mr-2"
             type="search"
             name="search"
             placeholder="Search"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
         />
     </form>
   )
--- src/components/tags/Tag.jsx
+++ src/components/tags/Tag.jsx
@@ -1,20 +1,32 @@
+import { useDispatch, useSelector } from "react-redux"
+import {tagSelected, tagsRemoved} from '../../features/filter/filterSlice'
+import { useEffect } from "react";
 
 export default function Tag({name}) {
   // {
-  //           "id": 1,
-  //           "name": "javascript"
-  //       },
+  //   "id": 1,
+  //   "name": "javascript"
+  // },
+  const dispatch = useDispatch();
+  const {tags: selectedTags} = useSelector(state => state.filters)
+  
+  const isSelected = selectedTags.includes(name) ? true : false;
+  const style = isSelected ? 'bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer' : 'bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer';
+  
+  const handleSelect = () => {
+    if(isSelected) {
+      dispatch(tagsRemoved(name));
+    } else {
+      dispatch(tagSelected(name));
+    }
+  }
+  
   return (
     <div
-        className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full cursor-pointer"
+      className={style}
+      onClick={handleSelect}
     >
-        {name}
+      {name}
     </div>
-                
-                // <div
-                //     className="bg-blue-600 text-white px-4 py-1 rounded-full cursor-pointer"
-                // >
-                //     redux
-                // </div>
   )
 }
